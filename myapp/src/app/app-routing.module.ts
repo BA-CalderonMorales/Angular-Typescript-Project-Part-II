db0e1c2fcb8091031f2a56effce5df1022abb812
BA_CalderonMorales <tbcm640@gmail.com>
2021-09-24 21:48:17 -0500

    Playing around with css and how it works within Angular. Loving it so far.

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccomplishmentsComponent } from './accomplishments/accomplishments.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'timeline', component: TimelineComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'accomplishments', component: AccomplishmentsComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AccomplishmentsComponent } from './accomplishments/accomplishments.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent,
+    AccomplishmentsComponent,
+    ProjectsComponent,
+    TimelineComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
